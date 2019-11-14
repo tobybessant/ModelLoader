b0fe015ac8860820a53c3f0e7dcdc20c8a3cff49
tobybessant <toby.bessant.1998@gmail.com>
2019-11-14 16:39:53 +0000

    Added loadModel method to main.

--- ModelLoader/ModelLoader.cpp
+++ ModelLoader/ModelLoader.cpp
@@ -16,13 +16,15 @@
 
 using namespace std;
 
-HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+void loadModel(std::string& modelPath, ObjReader& reader, std::vector<Model>& modelStore);
 
 int main(int argc, char** argv) {
 	// init service utilities for console and OBJ reader
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);	
 	ConsoleServices console = ConsoleServices(hConsole);
 	ObjReader oReader = ObjReader();
 
+	vector<Model> models;
 	string modelPath;
 
 	console.printStartup();
@@ -49,12 +51,15 @@ int main(int argc, char** argv) {
 			ShaderProgram program = ShaderProgram("media/triangles.vert", "media/triangles.frag");
 			program.use();
 
-			// read model file
-			Model model = Model();
-			oReader.parse(modelPath, model);
+			loadModel(modelPath, oReader, models);
+
 			while (!glfw.quit()) {
 				program.update();
-				model.render(program.id());
+				
+				for (int i = 0; i < models.size(); i++) {
+					models[i].render(program.id());
+				}
+
 				glfw.update();
 			}
 			glfw.destroy();
@@ -64,4 +69,11 @@ int main(int argc, char** argv) {
 			cout << "ERR: At least one invalid/non-existant model file found. Please remove and try again." << endl;
 		}
 	}
+}
+
+void loadModel(std::string& modelPath, ObjReader& reader, std::vector<Model>& modelStore) {
+	// read model file
+	Model model = Model();
+	reader.parse(modelPath, model);
+	modelStore.push_back(model);
 }
